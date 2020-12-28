b0600f91edca136afddad4c5ed9472efafb7762b
Ziad Zebib <zebibziad@gmail.com>
2020-12-28 21:00:08 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,8 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-
+  base_hash = {
+    :railroads => {}
+  }
+base_hash
 end
 
 def monopoly_with_second_tier
@@ -9,7 +10,17 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  base_hash = {
+    :railroads => {}
+  }
 
+    
+  base_hash[:railroads] = {
+    pieces: 4 ,
+    rent_in_dollars: {} , 
+    names: {}  
+  }
+  base_hash
 end
 
 def monopoly_with_third_tier
@@ -17,7 +28,32 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  base_hash = {
+    :railroads => {}
+  }
 
+    
+  base_hash[:railroads] = {
+    pieces: 4,
+    rent_in_dollars: {}, 
+    names: {}  
+  }
+  
+  base_hash[:railroads][:rent_in_dollars] = {
+    one_piece_owned: 25,
+    two_pieces_owned: 50,
+    three_pieces_owned: 100,
+    four_pieces_owned: 200
+  }
+
+  base_hash[:railroads][:names] = {
+    reading_railroad: {},
+    pennsylvania_railroad: {},
+    b_and_o_railroad: {},
+    shortline_railroad: {}
+  }
+  
+  base_hash  
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +61,46 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  base_hash = {
+    :railroads => {}
+  }
 
+    
+  base_hash[:railroads] = {
+    pieces: 4,
+    rent_in_dollars: {}, 
+    names: {}  
+  }
+  
+  base_hash[:railroads][:rent_in_dollars] = {
+    one_piece_owned: 25,
+    two_pieces_owned: 50,
+    three_pieces_owned: 100,
+    four_pieces_owned: 200
+  }
+
+  base_hash[:railroads][:names] = {
+    reading_railroad: {},
+    pennsylvania_railroad: {},
+    b_and_o_railroad: {},
+    shortline_railroad: {}
+  }
+  
+  base_hash[:railroads][:names][:reading_railroad] = {
+    mortgage_value: 100
+  }
+  
+  base_hash[:railroads][:names][:pennsylvania_railroad] = {
+    mortgage_value: 200
+  }
+  
+  base_hash[:railroads][:names][:b_and_o_railroad] = {
+    mortgage_value: 400
+  }
+  
+  base_hash[:railroads][:names][:shortline_railroad] = {
+    mortgage_value: 800
+  }
+  
+  base_hash
 end
